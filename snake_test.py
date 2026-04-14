5d99a99f0baf194b3b2a706bcdedc38cd77ce239
Pedrolucaspf <pedrolucas.portelafreitas@gmail.com>
2026-04-14 19:18:02 -0300

    :test_tube: test: Etapa Redc(falha): Testes de Movimento ao pressionar a tecla 'd'

---
+++ snake_test.py
@@ -0,0 +1,15 @@
+from snake import io_handler 
+import pytest
+
+@pytest.fixture
+def handler():
+    instance = io_handler((10, 15), 0.5)
+    instance.matrix[0][0] = 1 #corpo
+    instance.matrix[0][1] = 2 #cabeça
+    instance.matrix[0][2] = 3 #fruta
+    return instance
+
+def test_handler_mov_d(handler):
+    handler.last_input = 'd'
+    handler.movement()
+    assert handler.matrix[0][2] == 2
