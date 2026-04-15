fe23ccf1c76db4f2d66c1ac360a34ccf7afcae89
Pedrolucaspf <pedrolucas.portelafreitas@gmail.com>
2026-04-15 15:26:58 -0300

    Revert ":test_tube: test: Red: Teste de multiplas execu;"
    This reverts commit 4712a90d558c03a18967b0f2051557e6556b5ebe.

--- snake_test.py
+++ snake_test.py
@@ -1,5 +1,4 @@
-#from snake import io_handler
-from snake_ref import io_handler
+from snake import io_handler 
 import pytest
 
 @pytest.fixture
@@ -14,27 +13,3 @@ def test_handler_mov_d(handler):
     handler.last_input = 'd'
     handler.movement()
     assert handler.matrix[0][2] == 2
-    assert handler.matrix[0][1] == 1
-
-    handler.movement()
-    assert handler.matrix[0][3] == 2
-    assert handler.matrix[0][2] == 1
-
-    handler.last_input = 's'
-    handler.movement()
-    assert handler.matrix[1][3] == 2
-    assert handler.matrix[0][3] == 1
-
-    handler.movement()
-    assert handler.matrix[2][3] == 2
-    assert handler.matrix[1][3] == 1
-
-    handler.last_input = 'a'
-    handler.movement()
-    assert handler.matrix[2][2] == 2
-    assert handler.matrix[2][3] == 1
-
-    handler.last_input = 'w'
-    handler.movement()
-    assert handler.matrix[1][2] == 2
-    assert handler.matrix[2][2] == 1
