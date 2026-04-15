c96f398215de727a6f37ad6d9fdb915225892167
Pedrolucaspf <pedrolucas.portelafreitas@gmail.com>
2026-04-15 15:48:18 -0300

    :test_tube: test: Red: Teste de multiplas execuções do movimento e uso das quatro teclas

--- snake_test.py
+++ snake_test.py
@@ -1,4 +1,5 @@
-from snake import io_handler 
+#from snake import io_handler
+from snake_ref import io_handler
 import pytest
 
 @pytest.fixture
@@ -13,3 +14,27 @@ def test_handler_mov_d(handler):
     handler.last_input = 'd'
     handler.movement()
     assert handler.matrix[0][2] == 2
+    assert handler.matrix[0][1] == 1
+
+    handler.movement()
+    assert handler.matrix[0][3] == 2
+    assert handler.matrix[0][2] == 1
+
+    handler.last_input = 's'
+    handler.movement()
+    assert handler.matrix[1][3] == 2
+    assert handler.matrix[0][3] == 1
+
+    handler.movement()
+    assert handler.matrix[2][3] == 2
+    assert handler.matrix[1][3] == 1
+
+    handler.last_input = 'a'
+    handler.movement()
+    assert handler.matrix[2][2] == 2
+    assert handler.matrix[2][3] == 1
+
+    handler.last_input = 'w'
+    handler.movement()
+    assert handler.matrix[1][2] == 2
+    assert handler.matrix[2][2] == 1
