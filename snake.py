ca5eca0864eb1f79c4104814879714a70c7569f9
Pedrolucaspf <pedrolucas.portelafreitas@gmail.com>
2026-04-16 08:20:23 -0300

    :white_check_mark: test: Green - Cobra agora se move de acordo com as teclas

--- snake.py
+++ snake.py
@@ -3,13 +3,16 @@ import keyboard
 import time
 
 
+
 class io_handler:
     
     x_size: int
     y_size: int
-    game_speed = float
+    game_speed: float
     last_input: str
     matrix = []
+    head_x: int
+    head_y: int
 
     def __init__(self, dim, speed):
         self.x_size = dim[0]
@@ -17,6 +20,8 @@ class io_handler:
         
         self.game_speed = speed
         self.last_input = 'w'
+        self.head_x = 0
+        self.head_y = 1
 
         for i in range (self.y_size): 
             self.matrix.append([0]*self.x_size)
@@ -55,7 +60,24 @@ class io_handler:
         display_h_line(self)
     
     def movement(self):
-        self.matrix[0][2] = 2
+        i = self.head_x
+        j = self.head_y
+        if(self.last_input == 'w'):
+            self.matrix[i-1][j] = 2
+            self.matrix[i][j] = 1
+            self.head_x = i-1
+        elif(self.last_input == 'a'):
+            self.matrix[i][j-1] = 2
+            self.matrix[i][j] = 1
+            self.head_y = j-1
+        elif(self.last_input == 's'):
+            self.matrix[i+1][j] = 2
+            self.matrix[i][j] = 1
+            self.head_x = i+1
+        elif(self.last_input == 'd'):
+            self.matrix[i][j+1] = 2
+            self.matrix[i][j] = 1
+            self.head_y = j+1
             
 
 ### exemplo do uso da classe io_handler  
