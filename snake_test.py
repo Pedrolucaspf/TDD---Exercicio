7a339d914fe6445e571099b90cb5d2b0d3de2361
Pedrolucaspf <pedrolucas.portelafreitas@gmail.com>
2026-04-18 15:49:45 -0300

    :test_tube: tests: Red - Teste de movimento através das bordas

--- snake_test.py
+++ snake_test.py
@@ -10,7 +10,7 @@ def handler():
     instance.matrix[0][2] = 3 #fruta
     return instance
 
-def test_handler_mov_d(handler):
+def test_handler_mov(handler):
     handler.last_input = 'd'
     handler.movement()
     assert handler.matrix[0][2] == 2
@@ -38,3 +38,53 @@ def test_handler_mov_d(handler):
     handler.movement()
     assert handler.matrix[1][2] == 2
     assert handler.matrix[2][2] == 1
+
+@pytest.mark.parametrize("x_before, x_after, y_before, y_after, key", [
+    (0, 0, -1, 0, 'd'),
+    (-1, -1, 0, -1, 'a'),
+    (-1, 0, 0, 0, 's'),
+    (0, -1, -1, -1, 'w'),
+])
+
+def test_boundaries(x_before, x_after, y_before, y_after, key):
+    instance = io_handler((10, 15), 0.5)
+    instance.matrix[x_before][y_before] = 2
+    instance.last_input = key
+    instance.snake.head_x = x_before
+    instance.snake.head_y = y_before
+
+    if(key == 'd' or key == 's'):
+        change = 1
+    else:
+        change = -1
+
+    if(x_before == x_after):
+        instance.matrix[x_before][y_before-change] = 1
+        instance.movement()
+        assert instance.matrix[x_before][y_after] == 2
+        assert instance.matrix[x_before][y_before] == 1
+
+        for i in range(instance.x_size):
+            instance.movement()
+
+        assert instance.matrix[x_before][y_after] == 2
+        assert instance.matrix[x_before][y_after] == 1
+
+    elif(y_before == y_after):
+        instance.matrix[x_before-change][y_before] = 1
+        instance.movement()
+        assert instance.matrix[x_after][y_before] == 2
+        assert instance.matrix[x_before][y_before] == 1
+
+        for i in range(instance.y_size):
+            instance.movement()
+        
+        assert instance.matrix[x_after][y_before] == 2
+        assert instance.matrix[x_after][y_before] == 1
+
+
+
+
+
+    
+
