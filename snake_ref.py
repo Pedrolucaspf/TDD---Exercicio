dfb69e5b1c939926a43cc5bf04960988613a79da
Pedrolucaspf <pedrolucas.portelafreitas@gmail.com>
2026-05-03 22:38:59 -0300

    :recycle: refactor: Reimplementação de texto de pontuação final, limpeza de código comentado

--- snake_ref.py
+++ snake_ref.py
@@ -1,99 +1,109 @@
-import os
-import keyboard
-import time
+import pygame
 import random
+
+pygame.init()
+
+w = 640
+h = 640
+
+screen = pygame.display.set_mode((w, h))
+
+head_u_img = pygame.image.load('Graphics/head_up.png').convert()
+head_d_img = pygame.image.load('Graphics/head_down.png').convert()
+head_l_img = pygame.image.load('Graphics/head_left.png').convert()
+head_r_img = pygame.image.load('Graphics/head_right.png').convert()
+
+tail_u_img = pygame.image.load('Graphics/tail_up.png').convert()
+tail_d_img = pygame.image.load('Graphics/tail_down.png').convert()
+tail_l_img = pygame.image.load('Graphics/tail_left.png').convert()
+tail_r_img = pygame.image.load('Graphics/tail_right.png').convert()
+
+body_v_img = pygame.image.load('Graphics/body_vertical.png').convert()
+body_h_img = pygame.image.load('Graphics/body_horizontal.png').convert()
+body_tl_img = pygame.image.load('Graphics/body_topleft.png').convert()
+body_tr_img = pygame.image.load('Graphics/body_topright.png').convert()
+body_bl_img = pygame.image.load('Graphics/body_bottomleft.png').convert()
+body_br_img = pygame.image.load('Graphics/body_bottomright.png').convert()
+
+fruit_img = pygame.image.load('Graphics/apple.png').convert()
+
+side_len = head_u_img.get_width()
 
 class Snake_body:
     head_x: int
     head_y: int
     size: int
     body: list
+    seg_dir: list
+    head_hitbox: pygame.rect
+    body_hitboxes: list
 
     def __init__(self):
         self.head_x = 0
-        self.head_y = 1
+        self.head_y = 1 * side_len  
+        self.head_hitbox = pygame.Rect(self.head_x, self.head_y, side_len, side_len)
         self.size = 2
         self.body = [(0,0)]
+        self.seg_dir = ['h']
+        self.body_hitboxes = []
+        self.body_hitboxes.insert(0, pygame.Rect(self.body[0][0], self.body[0][1], side_len, side_len))
 
-
-class io_handler:
+class game:
     
     x_size: int
     y_size: int
-    fruit_count: int
-    game_speed: float
+    fruit_hitboxes: list
     last_input: str
     prev_input: str
-    test_reconstruct: bool
+    running: bool
     game_over: bool
-    matrix = []
     snake: Snake_body
 
-    def __init__(self, dim, speed):
+    def __init__(self, dim):
         self.x_size = dim[0]
         self.y_size = dim[1]
-        
-        self.fruit_count = 0
-        self.game_speed = speed
+        self.fruit_hitboxes = []
         self.last_input = 'd'
         self.prev_input = 'd'
         self.snake = Snake_body()
-        self.test_reconstruct = True
         self.game_over = False
-        self.matrix = []
+        self.running = True
 
-        for i in range (self.y_size): 
-            self.matrix.append([0]*self.x_size)
-
-    def record_inputs(self):
-        keyboard.add_hotkey('w', lambda: setattr(self, "last_input", 'w'))
-        keyboard.add_hotkey('a', lambda: setattr(self, "last_input", 'a'))
-        keyboard.add_hotkey('s', lambda: setattr(self, "last_input", 's'))
-        keyboard.add_hotkey('d', lambda: setattr(self, "last_input", 'd'))
-        keyboard.add_hotkey('esc', lambda: setattr(self, "last_input", 'end'))
-
-    def display(self):
-        def display_h_line(self):
-            print ('+', end='')
-            print ('--'* len(self.matrix[0]), end='')
-            print ('+')
+    def display(self, screen):
+        if(self.last_input == 'w'):
+            screen.blit(head_u_img, (self.snake.head_x, self.snake.head_y))
+        elif(self.last_input == 'a'):
+            screen.blit(head_l_img, (self.snake.head_x, self.snake.head_y))
+        elif(self.last_input == 's'):
+            screen.blit(head_d_img, (self.snake.head_x, self.snake.head_y))
+        elif(self.last_input == 'd'):
+            screen.blit(head_r_img, (self.snake.head_x, self.snake.head_y))
         
-        def display_content_line(line):
-            print ('|', end='')
-            for item in line: 
-                if item == 1:
-                    print ('[]', end='')
-                elif item == 2:
-                    print ('<>', end='')
-                elif item == 3:
-                    print ('()', end='')
-                else:
-                    print ('  ', end='')
+        for k in range(self.snake.size-1):
+            screen.blit(body_h_img, (self.snake.body_hitboxes[k].x, self.snake.body_hitboxes[k].y))
 
-            print ('|')
+        for k in range(len(self.fruit_hitboxes)):
+            screen.blit(fruit_img, (self.fruit_hitboxes[k].x, self.fruit_hitboxes[k].y))
 
-        os.system('cls' if os.name == 'nt' else 'clear')
-        display_h_line(self)
-        for line in self.matrix:
-            display_content_line(line)
-        display_h_line(self)
-    
     def movement(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+                return
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    self.last_input = 'w'
+                elif event.key == pygame.K_a:
+                    self.last_input = 'a'
+                elif event.key == pygame.K_s:
+                    self.last_input = 's'
+                elif event.key == pygame.K_d:
+                    self.last_input = 'd'
+                elif event.key == pygame.K_ESCAPE:
+                    self.game_over = True
+                    return
+
         if(self.game_over == False):
-
-            if(self.test_reconstruct):
-                self.snake.body = []
-                for x in range(self.y_size):
-                    for y in range(self.x_size):
-                        if(self.matrix[x][y] == 2):
-                            self.snake.head_x = x
-                            self.snake.head_y = y
-                        elif(self.matrix[x][y] == 1):
-                            self.snake.body.append((x, y))
-
-            i = self.snake.head_x
-            j = self.snake.head_y
-            erase_last = True
 
             if(self.prev_input == 's' and self.last_input == 'w'):
                self.last_input = 's'
@@ -104,108 +114,110 @@ class io_handler:
             elif(self.prev_input == 'd' and self.last_input == 'a'):
                self.last_input = 'd'
             else:
-                self.prev_input = self.last_input
+                self.prev_input = self.last_input       
 
+            i = self.snake.head_x
+            j = self.snake.head_y
+            erase_last = True
 
+            new_x = i
+            new_y = j
             if(self.last_input == 'w'):
-                new_x = (i-1)%self.y_size
-                if(self.matrix[new_x][j] == 1 and self.snake.body[-1]!=(new_x, j)):
-                    self.game_over = True
-                    return
-                elif(self.matrix[new_x][j] == 3):
-                    self.snake.size += 1
-                    erase_last = False
-                
-                self.matrix[new_x][j] = 2
-                self.matrix[i][j] = 1
-                self.snake.head_x = new_x
+                new_y = (j - side_len)%self.y_size
+
             elif(self.last_input == 'a'):
-                new_y = (j-1)%self.x_size
-                if(self.matrix[i][new_y] == 1 and self.snake.body[-1]!=(i, new_y)):
-                    self.game_over = True
-                    return
-                elif(self.matrix[i][new_y] == 3):
-                    self.snake.size += 1
-                    erase_last = False
+                new_x = (i - side_len)%self.x_size
 
-                self.matrix[i][new_y] = 2
-                self.matrix[i][j] = 1
-                self.snake.head_y = new_y
             elif(self.last_input == 's'):
-                new_x = (i+1)%self.y_size
-                if(self.matrix[new_x][j] == 1 and self.snake.body[-1]!=(new_x, j)):
-                    self.game_over = True
-                    return
-                elif(self.matrix[new_x][j] == 3):
-                    self.snake.size += 1
-                    erase_last = False
+                new_y = (j + side_len)%self.y_size
 
-                self.matrix[new_x][j] = 2
-                self.matrix[i][j] = 1
-                self.snake.head_x = new_x
             elif(self.last_input == 'd'):
-                new_y = (j+1)%self.x_size
-                if(self.matrix[i][new_y] == 1 and self.snake.body[-1]!=(i, new_y)):
+                new_x = (i + side_len)%self.x_size
+
+            new_head_hitbox = pygame.Rect(new_x, new_y, side_len, side_len)
+            
+            for k in range(len(self.snake.body_hitboxes)):
+                if(new_head_hitbox.colliderect(self.snake.body_hitboxes[k])):
+                    if(erase_last == True and k == len(self.snake.body_hitboxes)-1):
+                        continue
+
                     self.game_over = True
                     return
-                elif(self.matrix[i][new_y] == 3):
+                
+            for k in range(len(self.fruit_hitboxes) - 1, -1, -1):
+                if(new_head_hitbox.colliderect(self.fruit_hitboxes[k])):
                     self.snake.size += 1
-                    erase_last = False
+                    self.fruit_hitboxes.pop(k)
+                    erase_last = False            
 
-                self.matrix[i][new_y] = 2
-                self.matrix[i][j] = 1
-                self.snake.head_y = new_y
+            self.snake.head_x = new_x
+            self.snake.head_y = new_y
+            self.snake.head_hitbox = new_head_hitbox
 
             if(self.last_input != 'end'):
                 self.snake.body.insert(0, (i, j))
+                self.snake.body_hitboxes.insert(0, pygame.Rect(self.snake.body[0][0], self.snake.body[0][1], body_h_img.get_width(), body_h_img.get_height()))
                 if(erase_last == True):
-                    k = self.snake.body[-1][0]
-                    l = self.snake.body[-1][1]
-                    if(self.matrix[k][l]!=2):
-                        self.matrix[k][l] = 0
                     self.snake.body.pop()
-                else:
-                    self.fruit_count -= 1
-                    if(self.fruit_count < 0):
-                        self.fruit_count = 0
-
-                    if(self.fruit_count == 0):
+                    self.snake.body_hitboxes.pop()
+                elif(len(self.fruit_hitboxes) == 0):
                         self.spawn_fruit()
             
     def spawn_fruit(self):
         num_fruits = 1 + (self.snake.size//10) # Arredonda para baixo
         for i in range(num_fruits):
             fruit_spawned = 0
+            fruit = pygame.Rect(-1000, -1000, fruit_img.get_width(), fruit_img.get_height())
             while(fruit_spawned == 0):
-                x = random.randint(1, self.y_size) - 1
-                y = random.randint(1, self.x_size) - 1
-                if(self.matrix[x][y]<1 or self.matrix[x][y]>3):
-                    self.matrix[x][y] = 3
-                    self.fruit_count += 1
+                x = random.randint(2*side_len, self.y_size) - side_len
+                y = random.randint(2*side_len, self.x_size) - side_len
+                fruit.x = x
+                fruit.y = y
+                collided = 0
+                if(fruit.colliderect(self.snake.head_hitbox)):
+                    collided = 1
+                
+                for i in range(self.snake.size-1):
+                    if(fruit.colliderect(self.snake.body_hitboxes[i])):
+                        collided = 1
+                        break
+                    
+                if(collided == 0):
                     fruit_spawned = 1
-
-
-### exemplo do uso da classe io_handler  
-instance = io_handler((10,15), 0.5)
-instance.matrix[0][0] = 1 #corpo
-instance.matrix[0][1] = 2 #cabeça
-
+                    self.fruit_hitboxes.append(fruit)
 
 def game_loop():
-    instance.test_reconstruct = False
+    instance = game((w, h))
     instance.spawn_fruit()
-    instance.record_inputs()
-    while True:
-        instance.movement()
-        instance.display()
-        print("mova com WASD, saia com esc. Ultimo botão:", end=' ')
-        ###adicione seu código para lidar com o jogo aqui
-        
-        print(instance.last_input)
-        if(instance.game_over == True or instance.last_input == 'end'):
-            print("Pontuação Final: ", instance.snake.size)
-            exit()
-        time.sleep(instance.game_speed)
+
+    myfont = pygame.font.SysFont("monospace", 40)
+
+    clock = pygame.time.Clock()
+    
+    move_timer = 0
+    move_delay = 0.15  # segundos entre movimentos
+    delta_time = 0.1
+
+    while instance.running:
+        delta_time = clock.tick(60) / 1000
+        delta_time = max(0.001, min(0.1, delta_time))
+        move_timer += delta_time
+
+        if move_timer >= move_delay:
+            instance.movement() 
+            move_timer = 0
+
+        screen.fill((255, 255, 255))
+        instance.display(screen)
+
+        if(instance.game_over == True):
+            label = myfont.render(f"Pontuação final: {instance.snake.size}", 1, (0,255,0))
+            screen.blit(label, (w/6, h/3))
+
+        pygame.display.flip()
+
+    pygame.quit()
 
 if __name__ == "__main__":
     game_loop()
+
